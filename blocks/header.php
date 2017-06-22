aeff9d82b180993e4a3958e80e2e3dd7924fb56b
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-06-22 18:18:44 -0500

    Home

--- blocks/header.php
+++ blocks/header.php
@@ -1,3 +1,7 @@
-<header>
-	
+<header class="header">
+	<nav class="header-nav">
+		<h1 class="header-nav-logo">GIMNASIO</h1>
+		<img src="img/logo.png" class="header-nav-logo" alt="Logo">
+		<h1 class="header-nav-logo">BRITÁNICO</h1>
+	</nav>
 </header>
---
+++ blocks/home.php
@@ -0,0 +1,29 @@
+<!-- Seccion inicio principal -->
+<section class="homeStart">
+	<div class="homeStart-cta">
+		<div class="homeStart-cta-header">
+			<h2>Conoce nuestras instalaciónes. Agenda tu cita!</h2>
+		</div>
+		<form class="form-inline">
+			<div class="form-group">
+				<input type="text" class="form-control" name="nombrePadre" id="nombrePadre" placeholder="Nombre del padre" required/>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" name="nombreAlumno" id="nombreAlumno" placeholder="Nombre del Alumno" required/>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" name="edadAlumno" id="edadAlumno" placeholder="Edad del alumno" required/>
+			</div>
+			<div class="form-group">
+				<input type="text" class="form-control" name="curso" id="curso" placeholder="Curso al que aspira" required/>
+			</div>
+			<div class="form-group">
+				<input type="tel" class="form-control" name="telefono" id="telefono" placeholder="Teléfono de contacto" required/>
+			</div>
+			<div class="form-group">
+				<input type="email" class="form-control" name="email" id="email" placeholder="Email de contacto" required/>
+			</div>
+			<button type="submit" class="btn btn-sub">Contáctanos</button>
+		</form>
+	</div>
+</section>
