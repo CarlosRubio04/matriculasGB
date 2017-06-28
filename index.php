94ccd7336c4a6d42b0d135e25bf0ce2033635572
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-06-28 12:25:04 -0500

    chage cdn

--- index.php
+++ index.php
@@ -9,19 +9,20 @@
   <div class="loader" id="loader">
     <img src="img/loader.svg" alt="Loading...">
   </div>
-    <?php require_once('blocks/header.php'); ?>
-    <?php if(!isset($_REQUEST['content'])){
-      require_once('blocks/home.php');
-    }elseif($_REQUEST['content']=='gracias'){
-      require_once('blocks/gracias.php');
-    }else{
-      require_once('blocks/404.php');
-    } 
-    ?>
-    <?php require_once('blocks/footer.php'); ?>
+  <?php require_once('blocks/header.php'); ?>
+  <?php if(!isset($_REQUEST['content'])){
+    require_once('blocks/home.php');
+  }elseif($_REQUEST['content']=='gracias'){
+    require_once('blocks/gracias.php');
+  }else{
+    require_once('blocks/404.php');
+  } 
+  ?>
+  <?php require_once('blocks/footer.php'); ?>
   <!-- All js -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.32/jquery.form.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.11.1/jquery.validate.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <script src="js/jquery.bxslider.min.js"></script>
   <script src="js/bootbox.js"></script>
